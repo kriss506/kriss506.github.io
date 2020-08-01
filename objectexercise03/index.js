886030adee74e705dfc27a910d9ce9b5e23c87d9
Kriss Wilson <58282640+kriss506@users.noreply.github.com>
2020-08-01 14:27:15 +1000

    Add files via upload

---
+++ objectexercise03/index.js
@@ -0,0 +1,208 @@
+
+let tasks = [];
+
+function resetTasks() {
+    tasks = [];
+    for (let i = 0; i < 21; i++) {
+        tasks.push(randomTask(i));
+
+    }
+}
+
+const x = 0;
+function randomTask(id) {
+    const name = ["Buy groceries", "Go for a walk", "Prepare taxes", "Doctors appointment", "Attend show", "Watch movie", "Feed Dog", "Do laundry", "Test code", "Paint fence", "Inspect car", "Register car", "Insure car", "Ring Aunt Jane"];
+    const status = ['TODO', 'INPROGRESS', 'REVIEW', 'DONE'];
+    let ran = Math.floor(Math.random() * name.length);
+
+    let statusRandom = Math.floor(Math.random() * 4);
+
+    newTask = { id: id, name: name[ran], description: name[ran], assignedTo: "Kriss Wilson", dueDate: new Date(2020, 2, 1 + id, id), status: status[statusRandom] }
+    return newTask;
+}
+const taskManager = {
+
+    getTasks: function () {
+        return tasks;
+
+    }
+    ,
+    getTasksWithStatus: function (status) {
+
+        return tasks.filter(function (task) {
+            return task.status == status;
+        });
+
+    },
+    addTask: function (newTask) {
+        tasks.unshift(newTask);
+        return tasks;
+
+
+    },
+    deleteTask: function (taskToDelete) {
+        let taskIndex = tasks.findIndex(function (task) {
+            return task.id == taskToDelete.id;
+        });
+        //alert(taskIndex);
+        if (taskIndex != -1) {
+            let deletedItem = tasks.splice(taskIndex, 1);
+        }
+    },
+    getTaskIndexFromId: function (taskId) {
+        let taskIndex = tasks.findIndex(function (task) {
+            return task.id == taskId;
+        });
+        return taskIndex;
+    },
+    getTaskFromId: function (taskId) {
+        let taskIndex = tasks.findIndex(function (task) {
+            return task.id == taskId;
+        });
+        return tasks[taskIndex];
+    },
+
+    updateTaskStatus: function (taskId, newStatus) {
+        let t = null;
+        t = this.getTaskFromId(taskId);
+        if (t) {
+            t.status = newStatus;
+        }
+        console.log(tasks);
+
+    },
+    assignTask: function (taskId, newAssignee) {
+        let t = null;
+        t = this.getTaskFromId(taskId);
+        if (t) {
+
+            t.assignedTo = newAssignee;
+        }
+        console.log(tasks);
+    }
+
+}
+
+
+let t = taskManager.addTask({ id: 20, name: 'Go fishing', description: 'Go fishing', assignedTo: 'Sid Smith', dueDate: new Date(), status: 'TODO' });
+taskManager.deleteTask({ id: 20, name: 'Go fishing', description: 'Go fishing', assignedTo: 'Sid Smith', dueDate: new Date(), status: 'TODO' });
+taskManager.updateTaskStatus(1, "****STATUS****");
+taskManager.assignTask(2, "**new assignee**");
+console.log(tasks);
+console.log("finished");
+
+function updateOutput() {
+    const output = document.getElementById("output");
+    let st = "";
+    output.innerHTML = st;
+    let t = taskManager.getTasks();
+    for (let i = 0; i < t.length; i++) {
+        const element = t[i];
+        st = st + JSON.stringify(element) + "<br>";
+        let dynamicCard = document.createElement("DIV");
+        dynamicCard.classList.add("dynamicCard", element.status);
+        if (element.id == 10) { debugger; }
+        dynamicCard.innerHTML = element.id + ": " + element.name + ", " + element.status;
+        output.appendChild(dynamicCard);
+    }
+    //output.innerHTML += st;
+}
+
+function clearMainOuput() {
+    const output = document.getElementById("output");
+    let st = "";
+
+    output.innerHTML = st;
+}
+
+function updateOutput2(tasks) {
+    const output2 = document.getElementById("output2");
+    output2.innerHTML = "";
+    let st = "";
+    let t = tasks;
+    for (let i = 0; i < t.length; i++) {
+        const element = t[i];
+        st = st + JSON.stringify(element) + "<br>";
+        let dynamicCard = document.createElement("DIV");
+        dynamicCard.classList.add("dynamicCard", element.status);
+        dynamicCard.innerHTML = element.id + ": " + element.name + ", " + element.status;
+        output2.appendChild(dynamicCard);
+    }
+}
+
+
+//     output.innerHTML=st;;
+// }
+
+let ALLTASKS = null;
+let DONE = null;
+let TODO = null;
+let PROGRESS = null;
+let REVIEW = null;
+
+function resetChecks() {
+    ALLTASKS = taskManager.getTasks();
+    DONE = taskManager.getTasksWithStatus('DONE');
+    TODO = taskManager.getTasksWithStatus('TODO');
+    PROGRESS = taskManager.getTasksWithStatus('INPROGRESS');
+    REVIEW = taskManager.getTasksWithStatus('REVIEW');
+
+}
+function init() {
+
+    resetTasks();
+
+
+
+    let button01 = null;
+    let button02 = null;
+    let button03 = null;
+    let button04 = null;
+    let button05 = null;
+    let button06 = null;
+    let button07 = null;
+
+
+
+    button01 = document.getElementById("button01");
+    button01.addEventListener('click', () => { clearMainOuput(); resetChecks(); updateOutput(); });
+    button02 = document.getElementById("button02");
+    button02.addEventListener('click', () => { taskManager.addTask(randomTask(Math.floor(Math.random() * 1000))); updateOutput(); })
+    button03 = document.getElementById("button03");
+    button03.addEventListener('click', () => {
+        taskManager.deleteTask({ id: 10, name: 'Go fishing', description: 'Go fishing', assignedTo: 'Sid Smith', dueDate: new Date(), status: 'TODO' });
+        updateOutput()
+    });
+    button04 = document.getElementById("button04");
+    button04.addEventListener('click', () => { clearMainOuput(); resetTasks(); resetChecks(); updateOutput(); });
+
+    button05 = document.getElementById("button05");
+    button05.addEventListener('click', () => { resetChecks(); updateOutput2(DONE) });
+
+    button06 = document.getElementById("button06");
+    button06.addEventListener('click', () => { resetChecks(); updateOutput2(REVIEW) });
+
+    button07 = document.getElementById("button07");
+    button07.addEventListener('click', () => { resetChecks(); updateOutput2(PROGRESS) });
+    //updateOutput();
+}
+
+let button100 = null;
+dge(button100, "button100");
+
+function dge(button, buttonId) {
+    button = document.getElementById(buttonId);
+}
+
+let b = [button100];
+let bi = ["b100"];
+assignElements(b, bi);
+
+function assignElements(b, bi) {
+    for (let i = 0; i < b.length; i++) {
+        b[i] = document.getElementById(bi);
+
+    }
+}
+
+
